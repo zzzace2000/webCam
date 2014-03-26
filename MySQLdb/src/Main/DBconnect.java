573867560e5ed35912be31d5751f925074550a75
Kingsly <b99b01078@ntu.edu.tw>
2014-03-26 19:34:06 +0800

    Add mysql server

---
+++ MySQLdb/src/Main/DBconnect.java
@@ -0,0 +1,52 @@
+package Main;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+import com.mysql.jdbc.Driver;
+import com.mysql.jdbc.Statement;
+
+public class DBconnect {
+	
+	private Connection con;
+	private Statement st;
+	private ResultSet rs;
+	
+	public DBconnect() {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
+			st = (Statement) con.createStatement();
+			
+			
+		} catch (Exception ex) {
+			System.out.println("Erro: "+ex);
+		}
+		
+		
+		
+	}
+	
+	public void getData() {
+		try {
+			
+			String query = "select * from data";
+			rs = st.executeQuery(query);
+			
+			while(rs.next()) {
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				System.out.println("Name: "+name+", email: "+email);
+			}
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+	
+}
---
+++ MySQLdb/src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+public class Main {
+	
+	
+	public static void main(String srgs[]) {
+		
+		DBconnect connect = new DBconnect();
+		connect.getData();
+	}
+}
